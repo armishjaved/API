984edf4567f999b37cfc8e26ecc97ffe31540d74
Armish Javed <armishjaved@Armishs-MacBook-Pro.local>
2022-07-11 04:56:06 +0500

    Consumming a Public API

---
+++ code.js
@@ -0,0 +1,18 @@
+$(document).ready(()=>{
+   const sendreq = (name)=>{
+    const url = `https://api.genderize.io?name=${name}`
+    $.get(url,(response)=>{
+        if(response.gender == "male"){
+        $("#gender").text(response.gender+ "♂")
+        }
+        else{
+            $("#gender").text(response.gender+ "♀")
+        }
+    })
+   }
+   $("#form1").submit(function(x) {
+        x.preventDefault();
+        const name = $("#name-input").val();
+        sendreq(name);
+   })
+})
